096d286cadaa33dcf7a5a59042d642268178aa8f
Persona743 <siyavashshahidi7@gmail.com>
2023-04-10 10:49:20 +0430

    pages added

---
+++ pages/about-us.js
@@ -0,0 +1,22 @@
+const Page=() =>{
+    return (
+        <div>
+            <h1>About us Page</h1>
+            <p>
+                Commodo reprehenderit sint exercitation elit dolore tempor
+                consectetur. Elit amet velit non ea. Anim et aliquip qui
+                deserunt consectetur duis enim magna ad voluptate laboris
+                adipisicing. Irure eiusmod aute cupidatat sunt. Cupidatat esse
+                ex aute nisi dolor amet sint anim velit id laboris aute. Lorem
+                culpa magna mollit est veniam mollit eiusmod nisi proident
+                mollit incididunt.Tempor ipsum deserunt magna duis pariatur.
+                Irure ex ullamco consectetur nisi adipisicing nostrud cillum.
+                Reprehenderit velit ad duis est culpa officia cillum consectetur
+                enim ut. Enim consequat dolore deserunt ad aliquip amet fugiat
+                et velit.
+            </p>
+        </div>
+    );
+}
+
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,23 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>Events page</h1>
+            <div>
+                <a href="">
+                    <img />
+                    <h2>Events in Liverpool</h2>
+                </a>
+                <a href="">
+                    <img />
+                    <h2>Events in Texas</h2>
+                </a>
+                <a href="">
+                    <img />
+                    <h2>Events in Tokyo</h2>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
