44ecb096b9ef739a9cb65efb5aee3f4e5a8f3140
Persona743 <siyavashshahidi7@gmail.com>
2023-04-24 19:59:20 +0430

    single event added

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,8 +1,42 @@
-const EventPage=()=> {
-  return (
-    <h1>Our Single Event</h1>
-  )
-}
+import Image from 'next/image';
+
+const EventPage = ({ data }) => {
+    return (
+        <div>
+            <Image src={data.image} width={1000} height={500} alt={data.title} />
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+        </div>
+    );
+};
 
 export default EventPage;
 
+export async function getStaticPaths() {
+    const { allEvents } = await import('/data/data.json');
+
+    const allPaths = allEvents.map((path) => {
+        return {
+            params: {
+                cat: path.city,
+                //? name of this object should be the same as the file. like id for [id]
+                id: path.id,
+            },
+        };
+    });
+
+    return {
+        paths: allPaths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps(context) {
+    const id = context.params.id;
+    const { allEvents } = await import('/data/data.json');
+    const eventData = allEvents.find((item) => id === item.id);
+
+    return {
+        props: { data: eventData },
+    };
+}
